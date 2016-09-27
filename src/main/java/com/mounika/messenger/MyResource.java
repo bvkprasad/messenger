3da02ece0c54da95551233289ee86f78af5d6133
bvkprasad <prasadbulusu9@hotmail.com>
2016-09-28 00:27:17 +0530

    Update MyResource.java

--- src/main/java/com/mounika/messenger/MyResource.java
+++ src/main/java/com/mounika/messenger/MyResource.java
@@ -11,6 +11,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "love you kanna bangaram. You are my life laaaaa";
+        return "hello world";
     }
 }
